a3a988d780b1a31c63515d2d38bb6b25a4a04b63
LionZXY <nikita@kulikof.ru>
2021-07-10 17:32:25 +0300

    Add 1.7.10

---
+++ bridge-1.7/src/main/java/ru/glitchless/telegrambridge/TelegramBridgeMod.java
@@ -0,0 +1,17 @@
+package ru.glitchless.telegrambridge;
+
+import cpw.mods.fml.common.*;
+
+@Mod(
+        modid = TelegramBridgeMod.MODID,
+        name = "Telegram Bridge",
+        version = "${version}"
+)
+public class TelegramBridgeMod {
+    public static final String MODID = "telegrambridge";
+    public static final String UPDATE_URL = "https://www.curseforge.com/minecraft/mc-mods/telegram-bridge";
+
+    public TelegramBridgeMod() {
+
+    }
+}
